d5ecb5c349dc0b0158d1e58cb9abf63475563f6c
wynot1 <lhicks622@gmail.com>
2019-07-19 14:37:56 -0400

    built the HTML view files

--- src/shortcode/shortcodes.php
+++ src/shortcode/shortcodes.php
@@ -8,3 +8,7 @@
  * @link    http://example.com
  * @license MIT
  */
+
+//[qa question="What question would you like to ask me?" show_icon="" hide_icon-""]this is the answer[/qa]
+
+//[teaser visible_message="What question would you like to ask me?" show_icon="" hide_icon=""]this is the answer[/teaser]
